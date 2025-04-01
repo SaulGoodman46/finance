eb74d05b2887caf4578ee610a36119cbafaa16c5
SaulGoodman46 <s.strano1994@outlook.com>
2025-04-01 12:15:22 +0200

    added ping and chart pie plot

--- portfolio_manager/portfolio_manager_class.py
+++ portfolio_manager/portfolio_manager_class.py
@@ -1,10 +1,37 @@
 import bs4
 import requests
 import sys
-
-
+import subprocess
+import platform
+import matplotlib.pyplot as plt
+import numpy as np
 
 class portfolio_manager:
+
+    def __init__(self):
+        pass
+
+    def ping(self, host):
+        """
+        Ping a host to check for internet connectivity.
+        
+        Args:
+        host (str): The host to ping (e.g., 'google.com').
+        
+        Returns:
+        bool: True if the host is reachable, False otherwise.
+        """
+        # Determine the command based on the operating system
+        param = '-n' if platform.system().lower() == 'windows' else '-c'
+        
+        # Build the command
+        command = ['ping', param, '1', host]
+        
+        # Execute the command
+        response = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        # Check the return code
+        return response.returncode == 0
 
     def getFunds(self):
         file_name = "INPUTS/transactions.csv"
@@ -17,10 +44,10 @@ class portfolio_manager:
         transaction_dict = {}
         first = 0
         for line in lines:
+            strip_vect = line.strip(',')
             if (first == 0):
                 first = 1
-            else:
-                strip_vect = line.strip(',')
+            elif (len(strip_vect) > 4):
                 isin        = strip_vect[2]
                 type        = strip_vect[0]
                 stock_name  = strip_vect[1]
@@ -29,10 +56,9 @@ class portfolio_manager:
                 value       = strip_vect[5]
                 transaction_dict[isin] = [type, stock_name, date, quantity, value]
 
-        if sys.argv[-2] =='-ft':
-            url = 'https://markets.ft.com/data/funds/tearsheet/summary?s='
-        else:
-            url = 'https://www.boursorama.com/bourse/opcvm/cours/'
+        print(isin, type, stock_name, date, quantity, value)
+
+        url = 'https://markets.ft.com/data/funds/tearsheet/summary?s='
 
 
         #print(isin_codes)
@@ -61,6 +87,8 @@ class portfolio_manager:
                 name_list.append(name.text)
                 price_list.append(price.text.replace(',', ''))
 
+                print(f"Found ISIN {isin}, called {name} with price {price}\n")
+
                 #else:
                 #    name = soup_res.find('a',{'class' : 'c-faceplate__company-link'})
                 #    price = soup_res.find('span',{'class' : 'c-instrument c-instrument--last'})
@@ -72,10 +100,25 @@ class portfolio_manager:
                 name_list.append('NA')
                 price_list.append('NA')
                 continue
+    def plot_portfolio_chart(self, stock_dict):
+        
+        y = np.array(list(stock_dict.values()))
+        mylabels = list(stock_dict.keys())
 
 
-    def main(self):
-        self.getFunds()
+        plt.pie(y, labels = mylabels)
+        plt.show() 
 
-    if __name__=="__main__": 
-        main()
+## Main
+
+mng = portfolio_manager()
+if mng.ping('google.com'):
+    print("Internet connection is active.")
+    mng.getFunds()
+else:
+    print("Internet connection is down.")
+
+stock_dict = {}
+stock_dict["VanEk defence"] = 33
+stock_dict["Europe Stock 600"] = 67
+mng.plot_portfolio_chart(stock_dict)
